bdd21c688e3c833b709041ed76768b6a02440a61
samsuite <suitester@gmail.com>
2016-09-21 01:56:16 -0400

    ??????????????????

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -51,6 +51,8 @@ class PlayState extends FlxState
 	public var black_screen:FlxSprite;
 	public var white_screen:FlxSprite;
 	
+	public var sound_man:SoundManager;
+	
 	override public function create():Void
 	{
 		super.create();
@@ -115,6 +117,9 @@ class PlayState extends FlxState
 		
 		ui_man = new UIManager();
 		add(ui_man);
+		
+		sound_man = new SoundManager();
+		add(sound_man);
 		
 		white_screen = new FlxSprite(0,0);
 		white_screen.makeGraphic(1280, 720, FlxColor.WHITE);
--- source/SoundManager.hx
+++ source/SoundManager.hx
@@ -4,19 +4,29 @@ import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.group.FlxGroup;
 import flixel.util.FlxColor;
+import flixel.system.FlxSound;
 
 class SoundManager extends FlxGroup
 {
-	
+	public var scream_noise:FlxSound;
 	
 	public function new()
 	{
 		super();
+		scream_noise = FlxG.sound.load("assets/sounds/SCREAM.ogg");
+		trace("????");
+		scream_noise.play();
+		scream_noise.volume = 1;
+		trace (scream_noise.playing);
+		
+		//FlxG.sound.play("scream");
 	}
 	
 	override public function update(elapsed:Float):Void
 	{
-		
+		super.update(elapsed);
+		//scream_noise.play();
+		//trace("!!!");
 	}
 
 }
